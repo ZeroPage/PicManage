b4561750b98b0cada9d4b796c829cbc3365d35d0
Skywave <skywave@live.co.kr>
2014-11-21 17:04:39 +0900

    sample data and list implements

--- src/sp/gui/JGridList.java
+++ src/sp/gui/JGridList.java
@@ -1,8 +1,13 @@
 package sp.gui;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Arrays;
+
 import javax.swing.*;
 import javax.swing.event.*;
+
+import sp.imageinfo.ImageInfo;
 
 /**
  * @see http://stackoverflow.com/questions/7620579
@@ -11,14 +16,20 @@ import javax.swing.event.*;
 public class JGridList extends JPanel {
 
     private static final int N = 5;
-    private DefaultListModel dlm = new DefaultListModel();
-    private JList list = new JList(dlm);
+    private DefaultListModel<ImageInfo> dlm = new DefaultListModel<ImageInfo>();
+    private JList<ImageInfo> list = new JList<ImageInfo>(dlm);
 
     public JGridList() {
         super(new GridLayout());
         for (int i = 0; i < N * N; i++) {
-            String name = "Cell:" + String.format("%02d", i);
-            dlm.addElement(name);            
+            //String name = "Cell:" + String.format("%02d", i);
+            //dlm.addElement(name);   
+        	
+        	try {
+				dlm.addElement(new ImageInfo(new File("data/" + i + ".jpg")));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
         }
         list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
@@ -35,12 +46,19 @@ public class JGridList extends JPanel {
         }
 
         @Override
-        public Component getListCellRendererComponent(JList list, Object
-            value, int index, boolean isSelected, boolean cellHasFocus) {
-            JComponent jc =  (JComponent) super.getListCellRendererComponent(
-                list, value, index, isSelected, cellHasFocus);
-            jc.setBorder(BorderFactory.createEmptyBorder(N, N, N, N));
-            return jc;
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        	JLabel label =  (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        	ImageInfo info = (ImageInfo) value;        	
+        	
+    	    Image image = new ImageIcon(info.getFile().getAbsolutePath()).getImage().getScaledInstance(50, 50, 1);
+        	Icon icon = new ImageIcon(image);
+        	
+        	label.setBorder(BorderFactory.createEmptyBorder(N, N, N, N));
+    	    label.setIcon(icon);
+    	    label.setHorizontalTextPosition(JLabel.CENTER);
+    	    label.setVerticalTextPosition(JLabel.BOTTOM);
+    	    
+    	    return label;
         }
     }
 
@@ -52,14 +70,5 @@ public class JGridList extends JPanel {
                 System.out.println(Arrays.toString(list.getSelectedIndices()));
             }
         }
-    }
-
-    private void display() {
-        JFrame f = new JFrame("ListPanel");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(this);
-        f.pack();
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
     }
 }
