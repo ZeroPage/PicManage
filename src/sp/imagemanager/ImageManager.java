e62237c6c88eb0bc5e4bb1b2e8024fce6a16a60d
Hee Jeong <cho2hol@gmail.com>
2014-11-21 17:19:10 +0900

    ImageManager changed

--- src/sp/imagemanager/ImageManager.java
+++ src/sp/imagemanager/ImageManager.java
@@ -1,16 +1,19 @@
 package sp.imagemanager;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 import sp.imageinfo.ImageInfo;
 
-
 public class ImageManager implements Serializable {
-	private ArrayList ImageList;
+	private ArrayList<ImageInfo> ImageList;
 	private static final long serialVersionUID = 1L;
 	private String filePath;
 	
@@ -20,13 +23,14 @@ public class ImageManager implements Serializable {
 		loadImageList();
 	}
 	public void saveImageList() {
-		
-	}
-	public void loadImageList() {
 		try {
-			FileInputStream fileIn = new FileInputStream(filePath);
-			ObjectInputStream oIn = new ObjectInputStream(fileIn);
+			FileOutputStream fileOut = new FileOutputStream(filePath);
+			ObjectOutputStream oOut = new ObjectOutputStream(fileOut);
 			
+			oOut.writeObject(ImageList);
+			
+			oOut.close();
+			fileOut.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -36,8 +40,30 @@ public class ImageManager implements Serializable {
 		}
 		
 	}
-	public void addImage() {
+	public void loadImageList() {
+		try {
+			FileInputStream fileIn = new FileInputStream(filePath);
+			ObjectInputStream oIn = new ObjectInputStream(fileIn);
+			
+			ImageList = (ArrayList<ImageInfo>) oIn.readObject();
+			
+			fileIn.close();
+			oIn.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	public void addImage(File file) {
+		ImageInfo imageInfo = new ImageInfo(file);
+		ImageList.add(imageInfo);
 	}
 	public void deleteImage() {
 		
