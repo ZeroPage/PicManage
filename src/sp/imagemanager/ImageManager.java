8bbabf5995097b8e1e838d5adcca5a2665638b70
Hee Jeong <cho2hol@gmail.com>
2014-12-06 00:11:39 +0900

    add sort, getTagSet

--- src/sp/imagemanager/ImageManager.java
+++ src/sp/imagemanager/ImageManager.java
@@ -7,8 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.ListIterator;
+import java.util.*;
 
 import com.drew.imaging.jpeg.JpegProcessingException;
 import sp.imageinfo.ImageInfo;
@@ -16,6 +15,8 @@ import sp.imageinfo.ImageInfo;
 public class ImageManager implements Serializable {
 	private static final long serialVersionUID = -466251720671992920L;
 	private ArrayList<ImageInfo> imageList;
+	public static final int SORT_BY_NAME = 1;
+	public static final int SORT_BY_TIME = 2;
 	
 	public ImageManager() {
 		imageList = new ArrayList<ImageInfo>();
@@ -54,7 +55,45 @@ public class ImageManager implements Serializable {
 	public void deleteImage(ImageInfo imageInfo) {
 		imageList.remove(imageInfo);
 	}
-	public void sortImageList() {
+
+	public void sortImageList(int method) {
+		switch(method) {
+			case SORT_BY_NAME:
+				sortImageListByName();
+				break;
+			case SORT_BY_TIME:
+				sortImageListByTime();
+				break;
+			default:
+			 	break; //test
+		}
+	}
+	private void sortImageListByName() {
+		Collections.sort(imageList, new Comparator<ImageInfo>() {
+			@Override
+			public int compare(ImageInfo imageInfo1, ImageInfo imageInfo2) {
+				return imageInfo1.getFileName().compareTo(imageInfo2.getFileName());
+			}
+		});
+	}
+	private void sortImageListByTime() {
+		Collections.sort(imageList, new Comparator<ImageInfo>() {
+			@Override
+			public int compare(ImageInfo imageInfo1, ImageInfo imageInfo2) {
+				return imageInfo1.getFileTime().compareTo(imageInfo2.getFileTime());
+			}
+		});
+	}
+
+	//public ArrayList<ImageInfo> filterImageListByTag() {}
+
+	public HashSet<String> getTagSet() {
+		ListIterator<ImageInfo> it = imageList.listIterator();
+		HashSet<String> tagSet = new HashSet<String>();
+		while(it.hasNext()) {
+			tagSet.addAll(it.next().getTag());
+		}
+		return tagSet;
 	}
 
 }
