d89305312be963dc21a042d3153a741e7095bb1e
Hee Jeong <cho2hol@gmail.com>
2014-12-06 14:18:16 +0900

    public filter function to private

--- src/sp/imagemanager/ImageManager.java
+++ src/sp/imagemanager/ImageManager.java
@@ -101,7 +101,7 @@ public class ImageManager implements Serializable {
 		return clone;
 	}
 
-	public ArrayList<ImageInfo> filterImageListByTag(ArrayList<ImageInfo> clone, List<String> strings) {
+	private ArrayList<ImageInfo> filterImageListByTag(ArrayList<ImageInfo> clone, List<String> strings) {
 		ListIterator<ImageInfo> it = imageList.listIterator();
 		boolean flag = true;
 		while(it.hasNext()) {
